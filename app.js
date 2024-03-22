f09c504000ed3ec46f88903046144e323795b418
Greggy009 <85653474+Greggy009@users.noreply.github.com>
2024-03-21 21:09:15 -0700

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Habari Dunia!'));
+app.get('/', (req, res) => res.send('Habari Dunia! Karibuni Kenya. Swahili translation of Hello World! Welcome to Kenya'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
